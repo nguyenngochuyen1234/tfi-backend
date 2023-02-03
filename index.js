9e2253f55b10219c2ce89aebd8826f3e19272aaa
Huyen412 <ngochuyengiadinh4@gmail.orgg>
2023-02-03 16:30:33 +0700

    deploy 1

--- index.js
+++ index.js
@@ -16,7 +16,7 @@ const { Server } = require("socket.io");
 const httpServer = createServer(app);
 const io = new Server(httpServer, {
     cors: {
-        origin: "https://tfi-4db4lci00-nguyenngochuyen1234.vercel.app",
+        origin: "https://tfi-git-main-nguyenngochuyen1234.vercel.app",
     }
 });
 require('dotenv').config()
